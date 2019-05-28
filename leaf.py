f5ca76b50c3d34711509c003ea68ccde59ab5065
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-05-29 00:52:10 +0530

    Update leaf.py

--- leaf.py
+++ leaf.py
@@ -1,5 +1,8 @@
-year=int(input())
-if year%400==0 and year%4==0 or year%100==0:
-  print("Yes")
- else:
-  print("No")
+try:  
+  year=int(input())
+  if year%400==0 and year%4==0 or year%100==0:
+    print("yes")
+  else:
+    print("no")
+except:
+  print("give valid input")
