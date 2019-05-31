0227e2363a2a2282decb004ac43113bc665c3c7a
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-01 00:00:52 +0530

    Create prime no between range.py

---
+++ prime no between range.py
@@ -0,0 +1,12 @@
+#by Amirdharajan
+
+while True:
+    n,m=map(int ,input().split())#Which range you want to check, Give value1 value2
+    for i in range(n,m+1):
+        if i>1:
+            for j in range(2,i):# limits 2 to i times
+                if i%j == 0:
+                    break
+            else:
+                print(i)
+
