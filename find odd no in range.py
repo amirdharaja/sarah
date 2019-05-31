5a61f5eb6d37abcc7c3ccb0670a17d7d7147563a
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-01 00:15:09 +0530

    Create find odd no in range.py

---
+++ find odd no in range.py
@@ -0,0 +1,7 @@
+#by Amirdharajan
+
+n,m=map(int ,input().split())#Which range you want to check the ODD numbers, Give value1 value2
+for i in range(n+1,m):
+    if i%2!=0:
+        print(i,end=" ")
+
