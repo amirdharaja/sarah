2b1623bf2e260ec8afe2722cf213f00d890bccd7
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-09 01:40:28 +0530

    Create print, odd position has even and even position has odd vice versa.py

---
+++ print, odd position has even and even position has odd vice versa.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+length=len(l)
+for i in range(length):
+    if i%2==0 and l[i]%2!=0 or i%2!=0 and l[i]%2==0:
+        l1.append(l[i])
+for i in l1:
+    print(i,end=" ")
+
