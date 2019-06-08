13f8652e550c6fa10e69d15b7f3c67b3c6491cb5
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-09 01:45:31 +0530

    Update and rename print, odd positions has even and even position has odd vice versa.py to print, odd positions has even and even positions has odd vice versa.py

--- print, odd positions has even and even positions has odd vice versa.py
+++ print, odd positions has even and even positions has odd vice versa.py
@@ -1,8 +1,9 @@
 n=int(input())
 l=list(map(int,input().split()))
 l1=[]
-length=len(l)
-for i in range(length):
+for i in range(len(l)):
     if i%2==0 and l[i]%2!=0 or i%2!=0 and l[i]%2==0:
         l1.append(l[i])
-print(l1)
+for i in l1:
+    print(i,end=" ")
+
