b57fe7e0e7efaec803ee0eb58bee6aa577080b8d
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-09 10:52:06 +0530

    Update and rename reverseStrings.py to reverse string word by word.py

---
+++ reverse string word by word.py
@@ -0,0 +1,8 @@
+#by amirdharajan from KPR
+
+
+s=input()
+l=list(s.split())
+for i in l:
+    s=i[::-1]
+    print(s,end=" ")
--- reverseStrings.py
+++
@@ -1,6 +0,0 @@
-#by amirdharajan from KPR
-
-
-s=input()# give string input
-s=s[::-1] #slice method to print the string from the last index
-print(s)
