539153d6feb6886719f4a4232ccd6ee37139396d
amirdharaja <50826008+amirdharaja@users.noreply.github.com>
2019-06-09 11:14:21 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,15 @@
 
+def palindrome(str): 
+    for i in range(0, len(str)//2):  
+        if str[i] != str[len(str)-i-1]: 
+            return False
+    return True
+  
+
+s = input()
+ans = palindrome(s) 
+  
+if (ans): 
+    print("YES") 
+else: 
+    print("NO")
